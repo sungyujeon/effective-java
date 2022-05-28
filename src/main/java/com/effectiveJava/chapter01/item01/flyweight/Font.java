283d8cc05f2a7109eccdcd9d45e2bbb8e2cbbc4c
sungyujeon <sungyujeon91@gmail.com>
2022-05-28 16:16:39 +0900

    chore: 오타 수정

--- src/main/java/com/effectiveJava/chapter01/item01/flyweight/Font.java
+++ src/main/java/com/effectiveJava/chapter01/item01/flyweight/Font.java
@@ -1,6 +1,8 @@
 package com.effectiveJava.chapter01.item01.flyweight;
 
-public final class Font {
+import java.util.EnumSet;
+
+public class Font {
 
     final String family;
 
@@ -18,4 +20,42 @@ public final class Font {
     public int getSize() {
         return size;
     }
+}
+
+
+class FontImmutable extends Font {
+
+    private String v;
+
+    public FontImmutable(String family, int size) {
+        super(family, size);
+        v = "test";
+    }
+
+    public void setV(String val) {
+        this.v = val;
+    }
+
+    public static void main(String[] args) {
+        Font f = new FontImmutable("nanum", 1);
+
+        // immutable
+        Font f1 = new Font("hi", 1);
+        Font f2 = new Font("hi", 1);
+//        System.out.println(f1);
+//        System.out.println(f2);
+
+        MyEnum one = MyEnum.ONE;
+        System.out.println(one.getClass().getTypeName());
+
+        EnumSet enumSet = EnumSet.noneOf(MyEnum.class);
+        enumSet.add(MyEnum.ONE);
+//        enumSet.add(MyEnum.ONE);
+        System.out.println(enumSet.size());
+//        System.out.println(enumSet);
+    }
+}
+
+enum MyEnum {
+    ONE, TWO;
 }
